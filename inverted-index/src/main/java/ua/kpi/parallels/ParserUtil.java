2cc5ad33ea3eda0b43b12e4d62be2f51cae06d10
Veronica Velikaya <nik.velikay@gmail.com>
2021-05-25 23:30:10 +0300

    refactor(parser): inlined redundant variable terms, removed regex warnings for splitting

--- inverted-index/src/main/java/ua/kpi/parallels/ParserUtil.java
+++ inverted-index/src/main/java/ua/kpi/parallels/ParserUtil.java
@@ -28,13 +28,12 @@ public class ParserUtil {
             "will", "with", "would", "yet", "you", "your", "yours");
 
     public static Set<String> parseFile(String filePath) throws IOException {
-        Set<String> terms = Files.lines(Paths.get(filePath))
-                .map(line -> line.split("\\s+|\\.|\\,|\\;|\\:|\\-|\"|\\'|\\d|\\)|\\(|\\<|\\>|\\/|\\_|\\?|\\!|\\W|\\n|\\t"))
+        return Files.lines(Paths.get(filePath))
+                .map(line -> line.split("\\s+|\\.|,|;|:|-|\"|'|\\d|\\)|\\(|<|>|/|_|\\?|!|\\W|\\n|\\t"))
                 .flatMap(Arrays::stream)
                 .map(String::toLowerCase)
                 .filter(word -> !stopWords.contains(word))
                 .filter(word -> word.length()>2)
                 .collect(Collectors.toSet());
-        return terms;
     }
 }
