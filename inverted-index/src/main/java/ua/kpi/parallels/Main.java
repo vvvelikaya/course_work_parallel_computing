7264ffa60a6bdcb17cb3c37c8b0cfd8f674b3803
Veronica Velikaya <nik.velikay@gmail.com>
2021-05-26 20:08:59 +0300

    feat: calculating time of building index

--- inverted-index/src/main/java/ua/kpi/parallels/Main.java
+++ inverted-index/src/main/java/ua/kpi/parallels/Main.java
@@ -3,6 +3,8 @@ package ua.kpi.parallels;
 import ua.kpi.parallels.services.IndexService;
 
 import java.io.IOException;
+import java.time.Duration;
+import java.time.Instant;
 
 public class Main {
 
@@ -11,7 +13,10 @@ public class Main {
     public static void main(String[] args) {
         try {
             IndexService service = ConsoleController.chooseMode();
+            Instant start = Instant.now();
             service.buildIndex(invertedIndex);
+            Instant finish = Instant.now();
+            System.out.println("Time: " + Duration.between(start, finish).toMillis() + " ms");
             ConsoleController.readConsole(invertedIndex);
         } catch (IOException e) {
             System.out.println("I/O mistake");
