5bf4dd033152e7efed1f4ebf2ee8980a9a613070
Veronica Velikaya <nik.velikay@gmail.com>
2021-05-26 22:35:47 +0300

    feat!(controller): added input of several words

--- inverted-index/src/main/java/ua/kpi/parallels/ConsoleController.java
+++ inverted-index/src/main/java/ua/kpi/parallels/ConsoleController.java
@@ -8,6 +8,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashSet;
 import java.util.Set;
 
 public class ConsoleController {
@@ -17,7 +18,8 @@ public class ConsoleController {
     public static IndexService chooseMode() throws IOException {
         System.out.println("Choose mode:\n1 - serial\n2 - parallel");
         switch (reader.readLine()) {
-            case "1": return new SerialService();
+            case "1":
+                return new SerialService();
             case "2":
                 System.out.println("Enter number of indexing threads:");
                 return new ParallelService(Integer.parseInt(reader.readLine()));
@@ -29,18 +31,23 @@ public class ConsoleController {
 
     public static void readConsole(InvertedIndex index) throws IOException {
         String input;
-        System.out.println("Enter word to find (or 0 to exit):");
-        while(!(input = reader.readLine()).equals("0")) {
-            Set<File> docs = index.find(input);
-            if (docs != null) {
-                System.out.println("The word '" + input + "' found in " + docs.size() + " files:");
-                for (File doc : docs) {
-                    System.out.println(doc.getAbsolutePath());
-                }
-            } else {
-                System.out.println("This word is excluded as a stop word or doesn`t appear in indexed files");
+        System.out.println("Enter comma-separated words to find (or 0 to exit):");
+        while (!(input = reader.readLine()).equals("0")) {
+            Set<File> intersection = findIntersection(input, index);
+            System.out.println("'" + input + "' found in " + intersection.size() + " files");
+            for (File file : intersection) {
+                System.out.println(file);
             }
             System.out.println("Enter word to find (or 0 to exit):");
         }
+    }
+
+    private static Set<File> findIntersection(String input, InvertedIndex index) {
+        String[] words = input.split(", ");
+        Set<File> intersection = new HashSet<>(index.find(words[0]));
+        for (int i = 1; i < words.length; i++) {
+            intersection.retainAll(new HashSet<>(index.find(words[i])));
+        }
+        return intersection;
     }
 }
