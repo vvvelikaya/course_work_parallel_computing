3b3f4d176a0eda42c0ed62efc340890d0acbf11f
Veronica Velikaya <nik.velikay@gmail.com>
2021-05-26 19:09:08 +0300

    refactor!: user interaction using console controller

--- inverted-index/src/main/java/ua/kpi/parallels/Main.java
+++ inverted-index/src/main/java/ua/kpi/parallels/Main.java
@@ -1,40 +1,22 @@
 package ua.kpi.parallels;
 
-import java.io.File;
-import java.io.FileFilter;
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.BlockingQueue;
+import ua.kpi.parallels.services.IndexService;
+
+import java.io.IOException;
 
 public class Main {
 
-    static final File POISON = new File("");
-    static boolean stop = false;
-    final static int BOUND = 1000;
-    final static int N_CONSUMERS = 2;
-    static final File root = new File("D:\\Nika\\Документы\\data_for_indexing\\aclImdb");
-    static InvertedIndex index;
+    static InvertedIndex invertedIndex = new InvertedIndex();
 
     public static void main(String[] args) {
         try {
-            index = new InvertedIndex();
-            startIndexing();
+            IndexService service = ConsoleController.chooseMode();
+            service.buildIndex(invertedIndex);
+            ConsoleController.readConsole(invertedIndex);
+        } catch (IOException e) {
+            System.out.println("I/O mistake");
         } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public static void startIndexing() throws InterruptedException {
-        BlockingQueue<File> queue = new ArrayBlockingQueue<>(BOUND);
-        FileFilter filter = f -> {
-            if (!f.isDirectory()) {
-                return f.getName().endsWith("txt");
-            }
-            return true;
-        };
-
-        new Thread(new FileCrawler(queue, filter, root)).start();
-        for (int i = 0; i < N_CONSUMERS; i++) {
-            new Thread(new Indexer(queue, index)).start();
+            System.out.println("Some thread is interrupted");
         }
     }
 
